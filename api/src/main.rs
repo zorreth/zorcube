c035b5cec0393d3fd3eab5f429c2ad567ae0a64c
Kirill Siukhin <kirillsiukhin@proton.me>
2026-05-27 20:43:42 +0500

    feat: initialize axum and postgres service

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+
+    println!("Starting server at port 3000");
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "hello world"
 }
