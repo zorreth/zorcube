12e7d9612d5731b06d726e877bd2f5da2d7d1b66
Kirill Siukhin <kirillsiukhin@proton.me>
2026-06-02 07:16:37 +0500

    refactor: add Drizzle adapter

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+config({ path: '.env.local' });
+
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './src/schema.ts',
+  out: './drizzle',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
 import NextAuth from 'next-auth';
 import Discord from 'next-auth/providers/discord';
 import Google from 'next-auth/providers/google';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { db } from '@/schema';
 
 const handler = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [
     Discord({
       clientId: process.env.DISCORD_CLIENT_ID!,
